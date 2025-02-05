04b23fff5691d0b83a0fac03861d77dc88044361
MrTopik <topraktarikk@outlook.com>
2025-02-05 23:44:53 +0300

    changed weird name

---
+++ ALI/vscode/test2.py
@@ -0,0 +1,8 @@
+import time
+yaricap=float(input("Dairenin Yarıçapını Giriniz: "))
+pi=3.14
+alan=pi*(yaricap**2)
+cevre= 2*pi*yaricap
+print("Dairenin Alanı:", alan, "\nDairenin Çevresi: ", cevre)
+time.sleep(2.5)
+print("\nProgramı Kullandığınız İçin Minnettarız, İyi Günler Dileriz\n:)\n")
