7b5d488c7ee82056152c4bf44cf71cc3c49da31d
MrTopik <125857315+MrTopik@users.noreply.github.com>
2025-02-08 19:14:01 +0300

    Update les1_3.py
    Added English and language selection

--- VSCode/les1_3.py
+++ VSCode/les1_3.py
@@ -1,5 +1,16 @@
+lang = input("Language/Dil(English[Eng]/Turkce[Turk]): ")
+if lang == "Eng":
+  msg1 = "Enter radius: "
+  msg2 = "Area"
+  msg3 = "Circumference"
+elif lang == "Turk":
+  msg1 = "Yaricap girin: "
+  msg2 = "Alan"
+  msg3 = "Cevre"
+else:
+  print("This language is unknown/Bu dil bilinmiyor")
 pi = 3.14
-r = float(input("Yaricap girin: "))
+r = float(input(msg1))
 alan = pi * (r**2)
 cevre = 2*pi*r
-print(f"Alan = {alan}\nCevre = {cevre}")
+print(f"{msg2} = {alan}\n{msg3} = {cevre}")
