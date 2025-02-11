bdb19e105a8e30448fa152b28466bdb79d31d173
MrTopik <125857315+MrTopik@users.noreply.github.com>
2025-02-11 13:37:44 +0300

    Update ödev.py

--- dante/ödev.py
+++ dante/ödev.py
@@ -1,6 +1,6 @@
 viz = int(input("Vize puanınızı girin"))
 fin = int(input("Final puanınızı girin"))
-vize = viz*100/30
-fina = fin*100/70
+vize = viz*0.30
+fina = fin*0.70
 toplam =vize+fina
 print("Ders puanınız:" , toplam)
