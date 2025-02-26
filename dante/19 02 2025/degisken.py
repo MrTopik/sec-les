8d4c551e3c815bb5b83390c8dc6d573640482bec
SlayerOfGods0 <162009453+SlayerOfGods0@users.noreply.github.com>
2025-02-26 13:44:30 +0300

    Add files via upload

---
+++ dante/19 02 2025/degisken.py
@@ -0,0 +1,9 @@
+a = 10
+b = 5
+c = "hhy"
+pü = 3.14
+print(a , b , c , pü)
+
+
+ocak = mart = mayıs = temmuz = 30
+print (ocak)
---
+++ dante/19 02 2025/input.py
@@ -0,0 +1,6 @@
+a=int(input("Sayı girin:"))
+print(a)
+
+aa=int(input("sayı gir:"))
+aa=aa*aa
+print(aa)
